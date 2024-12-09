1410c380b41321bdd0d9afd1e2cc80539e830b7b
Matyáš Pilz <klanmptank@gmail.com>
2024-12-09 21:34:19 +0100

    Folder in standard
    Edited to standard Minetest / Luanti mod structure.

--- 10304/noderoads/init.lua
+++
@@ -1,8 +0,0 @@
--- Inicializace modu
-noderoads = {}
-
--- Načtení dalších souborů
-dofile(minetest.get_modpath("noderoads").."/roads.lua")
-dofile(minetest.get_modpath("noderoads").."/bus_stops.lua")
-dofile(minetest.get_modpath("noderoads").."/bridges.lua")
-dofile(minetest.get_modpath("noderoads").."/slopes.lua")
